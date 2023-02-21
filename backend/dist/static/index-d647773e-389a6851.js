9cab74ce97486586d744bf432038d99a4c8980fa
Sanjula Ranasinghe <86221845+Sanjula39@users.noreply.github.com>
2023-02-21 19:40:42 +0530

    sanity backend added

---
+++ backend/dist/static/index-d647773e-389a6851.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-9e58f0f1-a28e50dc.js";import"./desk-257f76bb-3f7bcece.js";import"./index-dd366e47.js";export{m as DocumentPaneProvider,a as default};
---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gflfhne6',
+    dataset: 'production'
+  }
+})
